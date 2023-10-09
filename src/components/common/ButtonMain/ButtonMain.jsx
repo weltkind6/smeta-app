cc5e0694df0a7f4b4e0309424800a3e6c57c58d3
yaroslavzefi <yaroslavzef@gmail.com>
2023-10-09 09:34:38 +0300

    main button change color

--- src/components/common/ButtonMain/ButtonMain.jsx
+++ src/components/common/ButtonMain/ButtonMain.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './styles.module.css'
 import PropTypes from "prop-types";
+import classnames from "classnames";
 
 const ButtonMain = ({isDiscount, discount}) => {
+    const [isClicked, setIsClicked] = useState(false);
+    const wrapperClassName = classnames(styles.wrapper, {
+        [styles.wrapper2]: isClicked,
+    });
 
     return (
-        <div className={styles.wrapper}>
-            <div>Добавить к заказу</div>
-            {!isDiscount ? <div>Бесплатно</div> : <div>Со скидкой {discount}%</div>}
+        <div className={wrapperClassName} onClick={() => setIsClicked(!isClicked)}>
+            {isClicked ?
+                <h5>Добавлено к заказу</h5>
+                :
+                <div>
+                    <div className={styles.title}>Добавить к заказу</div>
+                    {!isDiscount ? <div>Бесплатно</div> : <div>Со скидкой {discount}%</div>}
+                </div>
+            }
         </div>
     );
 };
